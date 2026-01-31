a722ed5f30d5b9013365aefee99010d41402c1ef
SaidiDhia <saididhia13@gmail.com>
2026-01-31 13:16:51 +0100

    Initialize messaging module structure

---
+++ PI_DEV/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.pi_dev {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.pi_dev to javafx.fxml;
+    exports com.example.pi_dev;
+}
